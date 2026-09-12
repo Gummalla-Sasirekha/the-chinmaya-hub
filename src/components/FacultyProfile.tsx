42f9409864894da94d29ee7c303281995893601c
Gummalla-Sasirekha <gummalla.cvv230163@cvv.ac.in>
2026-09-12 15:51:16 +0530

    Update FacultyProfile.tsx

--- src/components/FacultyProfile.tsx
+++ src/components/FacultyProfile.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from 'react'
 import type { Faculty, TimetableEntry } from '../types'
-import ExamDutyNotification from './ExamDutyNotification'
+<ExamDutyNotification faculty={faculty} />
 
 type Props = {
   faculty: Faculty
